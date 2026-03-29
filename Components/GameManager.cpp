7ecf182a322e536c1e6513a8cec173d5f4af04f6
ChrisB <92367999+Chris-Bejko@users.noreply.github.com>
2026-03-29 12:45:18 +0100

    Score to reset after pressing Main Menu

--- Components/GameManager.cpp
+++ Components/GameManager.cpp
@@ -4,10 +4,14 @@
 void GameManager::SetGameState(int _gameState)
 {
     gameState = _gameState;
-    if(!(environment && endUI && startUI)) return;
+    if (!(environment && endUI && startUI))
+        return;
 
     if (_gameState == 0)
     {
+        score = 0;
+        if (scoreText)
+            scoreText->SetText("Score: 0");
         environment->SetActive(false);
         endUI->SetActive(false);
         startUI->SetActive(true);
@@ -18,7 +22,7 @@ void GameManager::SetGameState(int _gameState)
         startUI->SetActive(false);
         environment->SetActive(true);
     }
-    else if(gameState == 2)
+    else if (gameState == 2)
     {
         finalScoreText->SetText("Final score: " + std::to_string(this->score));
         startUI->SetActive(false);
@@ -27,15 +31,15 @@ void GameManager::SetGameState(int _gameState)
     }
     else
     {
-        LOG_WARNING("Invalid game state for GameManager: ", _gameState , ". Valid options 0-2!");
+        LOG_WARNING("Invalid game state for GameManager: ", _gameState, ". Valid options 0-2!");
     }
 }
 
 void GameManager::AddToScore(int score)
 {
     this->score += score;
-    if (!scoreText) 
+    if (!scoreText)
         return;
-    
+
     scoreText->SetText("Score: " + std::to_string(this->score));
 }
